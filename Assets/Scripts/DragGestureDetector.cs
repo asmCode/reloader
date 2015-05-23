2d036ccc4e6e52dfed462ba82705e009eb3a3784
Szymon <Majewski>
2015-05-23 23:31:40 +0200

    handy gesture detector

---
+++ Assets/Scripts/DragGestureDetector.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragGestureDetector : MonoBehaviour
+{
+	private static DragGestureDetector m_instance;
+	private bool m_isTouching;
+	private Vector3 m_lastTouchPosition;
+
+	public event System.Action<Vector3> DragStarged;
+	public event System.Action<Vector3> DragMoved;
+	public event System.Action<Vector3> DragEnded;
+
+	public static DragGestureDetector Instance()
+	{
+		if (m_instance == null)
+		{
+			GameObject gameObject = new GameObject("DragGestureDetector", typeof(DragGestureDetector));
+			m_instance = gameObject.GetComponent<DragGestureDetector>();
+			DontDestroyOnLoad(gameObject);
+		}
+
+		return m_instance;
+	}
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0) && !m_isTouching)
+		{
+			m_isTouching = true;
+			m_lastTouchPosition = Input.mousePosition;
+
+			OnDragStarted(Input.mousePosition);
+		}
+		else if (Input.GetMouseButton(0) && m_isTouching)
+		{
+			if (m_lastTouchPosition != Input.mousePosition)
+				OnDragMoved(Input.mousePosition);
+
+			m_lastTouchPosition = Input.mousePosition;
+		}
+		else if (Input.GetMouseButtonUp(0) && m_isTouching)
+		{
+			m_isTouching = false;
+
+			OnDragEnded(Input.mousePosition);
+		}
+	}
+
+	protected void OnDragStarted(Vector3 touchPosition)
+	{
+		Debug.Log("OnDragStarted");
+
+		if (DragStarged != null)
+			DragStarged(touchPosition);
+	}
+
+	protected void OnDragMoved(Vector3 touchPosition)
+	{
+		Debug.Log("OnDragMoved");
+
+		if (DragMoved != null)
+			DragMoved(touchPosition);
+	}
+
+	protected void OnDragEnded(Vector3 touchPosition)
+	{
+		Debug.Log("OnDragEnded");
+
+		if (DragEnded != null)
+			DragEnded(touchPosition);
+	}
+}
