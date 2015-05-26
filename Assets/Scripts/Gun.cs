da32d303a7f9eb1c1520cb0d07bd2ebe0662aebb
Szymon <Majewski>
2015-05-27 01:09:26 +0200

    new Utils.cs

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -14,11 +14,15 @@ public class Gun : MonoBehaviour
 
 	private void SetReloadStage(int index)
 	{
+		if (m_currentReloadStageIndex != index)
+			m_reloadStages[m_currentReloadStageIndex].Leave();
+
 		for (int i = 0; i < m_reloadStages.Length; i++)
 			m_reloadStages[i].gameObject.SetActive(i == index);
 
 		m_reloadStages[index].ProgressChanged += HandleReloadStageProgressChanged;
 		m_reloadStages[index].Finished += HandleReloadStageFinished;
+		m_reloadStages[index].Enter();
 	}
 
 	private void NextStage()
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Utils
+{
+	public static float LinearDamp(float start, float end, float change)
+	{
+		float diff = end - start;
+
+		if (Mathf.Abs(diff) <= Mathf.Abs(change))
+			return 0.0f;
+
+		return start + change * Mathf.Sign(diff);
+	}
+
+}
