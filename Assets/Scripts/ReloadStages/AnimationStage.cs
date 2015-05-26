378ae00226e42d9b29fc14b57ea52a264cdde7d2
Szymon <Majewski>
2015-05-27 01:09:16 +0200

    new AnimationStage.cs

---
+++ Assets/Scripts/ReloadStages/AnimationStage.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationStage : ReloadStage
+{
+	public Animator m_animator;
+	public string m_triggerName;
+
+	public override void Enter()
+	{
+		m_animator.SetTrigger(string.IsNullOrEmpty(m_triggerName) ? "PlayTrigger" : m_triggerName);
+
+		Finish();
+	}
+}
